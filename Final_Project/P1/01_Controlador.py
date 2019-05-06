7d571de0976288b5554a6a4885ba71cf48682262
Pablo Ángel Mendoza Aguirre <pabloangelma@gmail.com>
2019-05-06 08:25:56 -0500

    carpetas y docs para tiempo real

---
+++ Final_Project/P1/01_Controlador.py
@@ -0,0 +1,14 @@
+# Code: 01_Controlador.py
+# -- 02_Registro
+exec(open("/Users/pablomendoza/Documents/ITESO/Trading/Lab_1/ITESO/MYST_LABs/Final_Project/P2/02_Registro.py").read())
+# -- 03_Inicializador
+exec(open("/Users/pablomendoza/Documents/ITESO/Trading/Lab_1/ITESO/MYST_LABs/Final_Project/P3/03_Inicializador.py").read())
+# -- 04_Colector
+exec(open("/Users/pablomendoza/Documents/ITESO/Trading/Lab_1/ITESO/MYST_LABs/Final_Project/P4/04_Colector.py").read())
+# -- 05_Algoritmo
+exec(open("/Users/pablomendoza/Documents/ITESO/Trading/Lab_1/ITESO/MYST_LABs/Final_Project/P5/05_Algoritmo.py").read())
+# -- 06_Trader
+exec(open("/Users/pablomendoza/Documents/ITESO/Trading/Lab_1/ITESO/MYST_LABs/Final_Project/P6/06_Trader.py").read())
+# -- 07_Mensajero
+exec(open("/Users/pablomendoza/Documents/ITESO/Trading/Lab_1/ITESO/MYST_LABs/Final_Project/P7/07_Mensajero.py").read())
+
